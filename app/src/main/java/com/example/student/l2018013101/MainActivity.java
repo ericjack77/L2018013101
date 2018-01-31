edeb640b9e8bb4f8af211563348a8c1543fe9330
ericchiu <ericjack7@gmail.com>
2018-01-31 14:47:04 +0800

    photo click2 存至外部空間

--- app/src/main/java/com/example/student/l2018013101/MainActivity.java
+++ app/src/main/java/com/example/student/l2018013101/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.student.l2018013101;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     ImageView img;
@@ -22,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(it,123);
     }
 
+    public void click2(View v)
+    {
+        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File f = new File(getExternalFilesDir("PHOTO"),"myphoto.jpg");
+        it.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+        startActivityForResult(it,456);
+    }
+
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -31,6 +45,18 @@ public class MainActivity extends AppCompatActivity {
             {
                 Bundle bundle = data.getExtras();
                 Bitmap bmp = (Bitmap) bundle.get("data");
+                img.setImageBitmap(bmp);
+            }
+        }
+
+        if (requestCode == 456)
+        {
+            if (resultCode == RESULT_OK)
+            {
+
+                //古早的方式  不過現在BMP檔案太大 使用此法記憶體容量易不足
+                File f = new File(getExternalFilesDir("PHOTO"),"myphoto.jpg");
+                Bitmap bmp = BitmapFactory.decodeFile(f.getAbsolutePath());
                 img.setImageBitmap(bmp);
             }
         }
